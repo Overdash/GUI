6331ab56794e0e512b507714128b5ee75ad7b044
Prodige <Prodigetukala@hotmail.co.uk>
2017-03-10 15:18:34 +0000

    Leggo!

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -118,6 +118,9 @@ export default class Index extends Component {
             <div id="master" class={ style.container }>
                 <div id="main">
                     <button class={style.cal} onClick={this.changeToMatch}>Match View</button>
+                    <button class={style.cal} onClick={this.handleClick} style={"left: 162px;"}>
+                        Login FB
+                    </button>
                     <button class={style.cal} style="left: 20px;" onClick={this.changeToKit}>Kit Check</button>
 
                     <div class={ style.details }></div>
@@ -176,7 +179,7 @@ export default class Index extends Component {
                     <button class={style.cal} onClick={this.changeBG} style={"left: 162px;"}>
                         Change Background
                     </button>
-                    <button class={style.cal} style={"left: 1%;"}>Enter Next Match</button>
+                    <button class={style.cal} style={"left: 17px;"}>Enter Next Match</button>
 
                     <div class={matchStyle.boxCont}>
                         <p>Conditions</p>
@@ -374,4 +377,90 @@ export default class Index extends Component {
     toRecommended(){
         window.open('https://www.amazon.co.uk/Sportswear-Outdoor-Clothing/b?ie=UTF8&node=116189031');
     }
+
+    //Facebook API stuff!
+    componentDidMount() {
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '281043865661978',
+                cookie     : true,  // enable cookies to allow the server to access
+                // the session
+                xfbml      : true,  // parse social plugins on this page
+                version    : 'v2.8' // use version 2.1
+            });
+
+            // Now that we've initialized the JavaScript SDK, we call
+            // FB.getLoginStatus().  This function gets the state of the
+            // person visiting this page and can return one of three states to
+            // the callback you provide.  They can be:
+            //
+            // 1. Logged into your app ('connected')
+            // 2. Logged into Facebook, but not your app ('not_authorized')
+            // 3. Not logged into Facebook and can't tell if they are logged into
+            //    your app or not.
+            //
+            // These three cases are handled in the callback function.
+            FB.getLoginStatus(function(response) {
+                this.statusChangeCallback(response);
+            }.bind(this));
+        }.bind(this);
+
+        // Load the SDK asynchronously
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    };
+
+// Here we run a very simple test of the Graph API after login is
+// successful.  See statusChangeCallback() for when this call is made.
+/*
+    testAPI = function() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+            console.log('Successful login for: ' + response.name);
+            document.getElementById('status').innerHTML =
+                'Thanks for logging in, ' + response.name + '!';
+        });
+    };
+*/
+
+// This is called with the results from from FB.getLoginStatus().
+    statusChangeCallback = function(response) {
+        console.log('statusChangeCallback');
+        console.log(response);
+        // The response object is returned with a status field that lets the
+        // app know the current login status of the person.
+        // Full docs on the response object can be found in the documentation
+        // for FB.getLoginStatus().
+        if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+            this.testAPI();
+        } else if (response.status === 'not_authorized') {
+            // The person is logged into Facebook, but not your app.
+            document.getElementById('status').innerHTML = 'Please log ' +
+                'into this app.';
+        } else {
+            // The person is not logged into Facebook, so we're not sure if
+            // they are logged into this app or not.
+            document.getElementById('status').innerHTML = 'Please log ' +
+                'into Facebook.';
+        }
+    };
+
+// This function is called when someone finishes with the Login
+// Button.  See the onlogin handler attached to it in the sample
+// code below.
+    checkLoginState = function() {
+        FB.getLoginStatus(function(response) {
+            this.statusChangeCallback(response);
+        }.bind(this));
+    };
+
+    handleClick =  function() {
+        FB.login(this.checkLoginState);
+    };
 }
